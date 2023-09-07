3e79afd2a883474bf5bc20671bc9512bbb892e91
Евгений Егоров <evstroyka@gmail.com>
2023-09-07 12:36:52 +0300

    Добавьте файлы проекта.

---
+++ MVC/Controllers/CalcController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using System.Diagnostics;
+
+namespace MVC.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(int a, int b, string c)
+        {
+            switch (c)
+            {
+                case null:
+                case "+": return $"{a} + {b} = {a + b}";
+                case "-": return $"{a} - {b} = {a - b}";
+                case "*": return $"{a} * {b} = {a * b}";
+                case "/": return b != 0 ? $"{a} / {b} = {a / b}" : "Делить на ноль нельзя"; // Тернарный оператор
+                default:
+                    return "Можно передовать только операции +,-,*";
+            }
+        }
+    }
+}
