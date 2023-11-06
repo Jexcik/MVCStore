ce4f9a8cda9c1266b0456f4e20ddf15e3d695ed7
Евгений Егоров <evstroyka@gmail.com>
2023-11-06 23:52:00 +0300

    commit

--- MVC/Areas/Admin/Controllers/UserController.cs
+++ MVC/Areas/Admin/Controllers/UserController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Areas.Admin.Models;
+using MVC.Models;
 using System.Reflection.Metadata.Ecma335;
 
 namespace MVC.Areas.Admin.Controllers
@@ -16,12 +18,99 @@ namespace MVC.Areas.Admin.Controllers
 
         public IActionResult Index()
         {
-            var users=usersRepository.GetAll();
+            var users = usersRepository.GetAll();
             return View(users);
         }
-        public IActionResult Add() 
+        public IActionResult Add()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Register register)
+        {
+            var userAccount = usersRepository.TryGetByName(register.UserName);
+            if (userAccount != null)
+            {
+                ModelState.AddModelError("", "Пользователь с таким именем уже есть.");
+                return View(register);
+            }
+            if (register.UserName == register.Password)
+            {
+                ModelState.AddModelError("", "Имя пользователя и пароль не должны совпадать");
+                return View(register);
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(register);
+            }
+            usersRepository.Add(new User(register.UserName, register.Password, register.FirstName, register.LastName, register.Phone));
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Details(Guid userId)
+        {
+            var user = usersRepository.TryGetById(userId);
+            return View(user);
+        }
+
+        public IActionResult Del(Guid userId)
+        {
+            usersRepository.Del(userId);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(Guid userId)
+        {
+            var user = usersRepository.TryGetById(userId);
+            var editUser = new EditUser();
+            editUser.UserName = user.Name;
+            editUser.FirstName = user.FirstName;
+            editUser.LastName = user.LastName;
+            editUser.Phone = user.Phone;
+            ViewData["userId"] = userId;
+            return View(editUser);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditUser user, Guid userId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(user);
+            }
+            usersRepository.Edit(user, userId);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult ChangePassword(Guid userId)
+        {
+            var user = usersRepository.TryGetById(userId);
+            ViewData["userId"] = userId;
+            ViewData["userName"] = user.Name;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ChangePassword(Guid userId, string password)
+        {
+            usersRepository.ChangePassword(userId, password);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult ChangeAccess(Guid userId)
+        {
+            var user = usersRepository.TryGetById(userId);
+            var roles = rolesRepository.GetAllRoles();
+            ViewData["userId"] = userId;
+            ViewData["userName"] = user.Name;
+            ViewData["userRole"] = user.Role.Name;
+            return View(roles);
+        }
+
+        [HttpPost]
+        public IActionResult ChangeAccess(Guid userId, string role)
+        {
+            usersRepository.ChangeAccess(userId, role);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
